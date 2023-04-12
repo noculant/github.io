15c9c3c4523f33f337269543cbc64ea5a05810a5
jose reyes <jreyes1104@gmail.com>
2023-04-12 15:56:37 -0700

    removed unneccesary functions

--- expense_record_class.py
+++ expense_record_class.py
@@ -11,17 +11,5 @@ class ExpenseRecord:
         }
         self.expense_records.append(expense)
 
-    def get_expenses_by_category(self, category):
-        expenses = [record for record in self.expense_records if record['category'] == category]
-        return expenses
-
-    def get_total_expense_by_category(self, category):
-        expenses = self.get_expenses_by_category(category)
-        total_expense = sum([expense['amount'] for expense in expenses])
-        return total_expense
-
-    def get_all_expenses(self):
-        return self.expense_records
-
     def get_total_expenses(self):
         return sum([record['amount'] for record in self.expense_records])
--- tenant_class.py
+++ tenant_class.py
@@ -3,18 +3,3 @@ class Tenant:
         self.name = name
         self.apartment_number = apartment_number
         self.rate = rate
-
-    def get_name(self):
-        return self.name
-
-    def get_apartment_number(self):
-        return self.apartment_number
-
-    def get_rate(self):
-        return self.rate
-
-    def set_name(self, name):
-        self.name = name
-
-    def set_rate(self, rate):
-        self.rate = rate
