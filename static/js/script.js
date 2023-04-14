3640f30765286986e6deea865659a303b45ed101
jose reyes <jreyes1104@gmail.com>
2023-04-13 20:32:43 -0700

    fixed comments and added them to javascript file

--- static/js/script.js
+++ static/js/script.js
@@ -1,17 +1,17 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const form = document.getElementById("data-form");
-    const messageBox = document.createElement("div");
+document.addEventListener("DOMContentLoaded", function () { // Sets up event listener that exectures once content is loaded
+    const form = document.getElementById("data-form"); // Selects data-form ID element
+    const messageBox = document.createElement("div"); // This line creates a new div element
   
-    form.addEventListener("submit", async function (event) {
-      event.preventDefault();
+    form.addEventListener("submit", async function (event) { // Sets up event listener for submit
+      event.preventDefault(); // Prevents the default form from reloading the page
   
-      const formData = new FormData(form);
-      const response = await fetch("/", {
-        method: "POST",
-        body: formData,
+      const formData = new FormData(form); // Creates a formData object to sent to server
+      const response = await fetch("/", { // Sends a asynchronous request to the server using fetch function
+        method: "POST", // Sets up HTTP request method to post
+        body: formData, // Sets the request body to formData
       });
-      const result = await response.json();
+      const result = await response.json(); // Reads the response from the server as JSON
   
-      if (result.status === "success") {
+      if (result.status === "success") { // Checks whether server response was a success
         messageBox
   
